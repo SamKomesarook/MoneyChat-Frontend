fdcbefe14600bb4d1f7033a217252254704855f3
David Manolitsas <davidmanolitsas@icloud.com>
2020-08-25 11:21:34 +1000

    Adding contacts view on behalf of Hardik

--- lib/view/contacts_view.dart
+++ lib/view/contacts_view.dart
@@ -9,24 +9,180 @@ class Contacts extends StatefulWidget {
 class _ContactsState extends State<Contacts> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Contacts'),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => AddContact()),
-          );
-        },
-      ),
+    final title = '';
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: title,
+      home: Scaffold(
+          body: ListView(
+            children: <Widget>[
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.purple,
+                ),
+                title: Text(
+                  'John Doe',
+                ),
+                subtitle: Text('0123456789'),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                onTap: () {
+                  Text('Another data');
+                },
+              ),
+              new Divider(
+                height: 1.0,
+                indent: 1.0,
+              ),
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.blue,
+                ),
+                title: Text(
+                  'John Doe',
+                ),
+                subtitle: Text('0123456789'),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                onTap: () {
+                  Text('Another data');
+                },
+              ),
+              new Divider(
+                height: 1.0,
+                indent: 1.0,
+              ),
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.green,
+                ),
+                title: Text(
+                  'John Doe',
+                ),
+                subtitle: Text('0123456789'),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                onTap: () {
+                  Text('Another data');
+                },
+              ),
+              new Divider(
+                height: 1.0,
+                indent: 1.0,
+              ),
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.black,
+                ),
+                title: Text(
+                  'John Doe',
+                ),
+                subtitle: Text('0123456789'),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                onTap: () {
+                  Text('Another data');
+                },
+              ),
+              new Divider(
+                height: 1.0,
+                indent: 1.0,
+              ),
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.red,
+                ),
+                title: Text(
+                  'John Doe',
+                ),
+                subtitle: Text('0123456789'),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                onTap: () {
+                  Text('Another data');
+                },
+              ),
+              new Divider(
+                height: 1.0,
+                indent: 1.0,
+              ),
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.orange,
+                ),
+                title: Text(
+                  'John Doe',
+                ),
+                subtitle: Text('0123456789'),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                onTap: () {
+                  Text('Another data');
+                },
+              ),
+              new Divider(
+                height: 1.0,
+                indent: 1.0,
+              ),
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.pink,
+                ),
+                title: Text(
+                  'John Doe',
+                ),
+                subtitle: Text('0123456789'),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                onTap: () {
+                  Text('Another data');
+                },
+              ),
+              new Divider(
+                height: 1.0,
+                indent: 1.0,
+              ),
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.blue,
+                ),
+                title: Text(
+                  'John Doe',
+                ),
+                subtitle: Text('0123456789'),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                onTap: () {
+                  Text('Another data');
+                },
+              ),
+              new Divider(
+                height: 1.0,
+                indent: 1.0,
+              ),
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                ),
+                title: Text(
+                  'John Doe',
+                ),
+                subtitle: Text('0123456789'),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                onTap: () {
+                  Text('Another data');
+                },
+              ),
+              new Divider(
+                height: 1.0,
+                indent: 1.0,
+              ),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.white,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AddContact()),
+              );
+            },
+            child: Icon(Icons.add),
+          )),
     );
   }
 }
